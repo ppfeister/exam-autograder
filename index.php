e37aa5e7de0f461a7d2506a418e4da3fdf484ffd
Paul Pfeister <paul@pfeister.dev>
2021-10-16 00:36:02 -0400

    Begin front end

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Coding examination website with automatic grading of student-submitted code">
+    <title>Coding Exam Software</title>
+    <link rel="stylesheet" href="styles/master.css" type="text/css">
+</head>
+<body>
+    <header>
+        <h1>Code exam</h1>
+    </header>
+    <div>
+        <p>Sign in</p>
+        <form>
+            <input type="text" placeholder="Username">
+            <input type="password" placeholder="Password">
+        </form>
+        <button type="submit" form="sign-in" value="Submit">Sign in</button>
+        <button>Sign up</button>
+    </div>
+</body>
+</html>
