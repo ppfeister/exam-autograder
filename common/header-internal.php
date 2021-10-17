9bc03bba39b810b901822e8bf1786e7a28787599
Paul Pfeister <paul@pfeister.dev>
2021-10-17 07:21:16 -0400

    php-ify headers

---
+++ common/header-internal.php
@@ -0,0 +1,3 @@
+<header>
+    <div><h1><a href="">bitlab</a></h1></div>
+</header>
--- index.php
+++ index.php
@@ -4,20 +4,13 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Coding examination website with automatic grading of student-submitted code">
-    <title>Coding Exam Software</title>
+    <title>bitlab - Student Coding Lab</title>
     <link rel="stylesheet" href="styles/master.css" type="text/css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Sharp" rel="stylesheet">
 </head>
 <body>
-    <header>
-        <div><h1><a href="">bitlab</a></h1></div>
-        <div id="login-toolbar">
-            <form>
-                <input type="text" placeholder="Username">
-                <input type="password" placeholder="Password">
-                <button type="submit"><span class="material-icons-sharp">login</span></button>
-            </form>
-        </div>
-    </header>
+<?php
+require_once('common/header-login.php');
+?>
 </body>
 </html>
