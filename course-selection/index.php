49f934b2da22471e89b2dd2b9e669addbcf1bb47
Paul Pfeister <paul@pfeister.dev>
2021-10-17 21:44:22 -0400

    Continue course selection page

--- course-selection/index.php
+++ course-selection/index.php
@@ -12,9 +12,46 @@
 <?php
 require_once('../common/header-internal.php');
 ?>
+    <!--<div class="section-menu"></div>-->
     <div class="section-main">
-        <div>stuff</div>
-        <div>stuff 2</div>
+        <div class="course-selection-block first-item">
+            <div class="course-info">
+                <span class="course-name">CS 490</span>
+            </div>
+            <div class="course-assignment-list">
+                <div class="assignment-listing">
+                    <span class="assignment-name">Milestone 1</span>
+                    <span class="assignment-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit</span>
+                    <span class="assignment-duedate">Oct 20, 2021 11:59pm<span class="material-icons-sharp status-icon">task</span></span>
+                </div>
+                <div class="assignment-listing">
+                    <span class="assignment-name">Milestone 1</span>
+                    <span class="assignment-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit</span>
+                    <span class="assignment-duedate status-icon">Oct 20, 2021 11:59pm<span class="material-icons-sharp status-icon">task</span></span>
+                </div>
+                <div class="assignment-listing">
+                    <span class="assignment-name">Milestone 1</span>
+                    <span class="assignment-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit</span>
+                    <span class="assignment-duedate">Oct 20, 2021 11:59pm<span class="material-icons-sharp status-icon">hourglass_bottom</span></span>
+                </div>
+            </div>
+        </div>
+        <div class="course-selection-block">
+            <div class="course-info">
+                <span class="course-name">CS 490</span>
+            </div>
+            <div class="course-assignment-list">
+                assignments go here
+            </div>
+        </div>
+        <div class="course-selection-block last-item">
+            <div class="course-info">
+                <span class="course-name">CS 490</span>
+            </div>
+            <div class="course-assignment-list">
+                assignments go here
+            </div>
+        </div>
     </div>
 </body>
 </html>
