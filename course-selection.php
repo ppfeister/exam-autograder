000ef28e79a715c8f02bb03116ebc93068a8251c
Ahmed Abdullah Faisal <ahmedabdulfaisal@gmail.com>
2021-10-30 12:18:27 -0400

    added files

---
+++ course-selection.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Coding examination website with automatic grading of student-submitted code">
+    <title>Coding Exam Software</title>
+    <link rel="stylesheet" href="styles/master.css" type="text/css">
+</head>
+<body>
+    <header> <!-- title -->
+        <h1><a href="">bitlab</a></h1>
+    </header>
+
+    <p><!-- course menu -->
+        <h2>My Courses</h2>
+    </p>
+
+    <div>
+        <h3>Course #</h3>
+        <h4>Course Professor</h4>
+        <h4>Course Description</h4>
+    </div>
+
+    <div>
+        <h3>Course #</h3>
+        <h4>Course Professor</h4>
+        <h4>Course Description</h4>
+    </div>
+
+    <div>
+        <h3>Course #</h3>
+        <h4>Course Professor</h4>
+        <h4>Course Description</h4>
+    </div>
+
+    <div>
+        <h3>Course #</h3>
+        <h4>Course Professor</h4>
+        <h4>Course Description</h4>
+    </div>
+    
+    
+</body>
+</html>
