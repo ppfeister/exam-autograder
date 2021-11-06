ccf68c1bcb4d3e8ba59b989159fadcb42c669955
Ahmed Faisal <80870776+AhmedAbdulFaisal@users.noreply.github.com>
2021-11-06 17:51:48 -0400

    added new auth.php, in common

---
+++ common/authn.php
@@ -0,0 +1,36 @@
+<!-- Hypothetical Table Code
+    CREATE TABLE users (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+--> 
+
+<?php
+
+require_once 'dbconfig.php';
+
+if(isset($_POST['but_submit'])) {
+    $uname = mysqli_real_escape_string($con,$_POST['txt_uname']);
+    $password = mysqli_real_escape_string($con,$POST['txt_pwd']);
+
+    if ($uname != "" && $password != "") {
+        //check for passwords within system
+
+        $sql_query = "select count(*) as cntUser 
+            from user 
+            where username='".$uname."' and password='".$password."'";
+        $resuilt = mysqli_query($con,$sql_query);
+        $row = mysqli_query($con,$sql_query);
+        $count = $row['cntUser'];
+
+        if ($count > 0) {
+            //set page to courses page
+        }else {
+            echo "wrong password lmao";
+        }
+    }
+}
+
+?>
--- common/header-login.php
+++ common/header-login.php
@@ -2,8 +2,8 @@
     <div><h1><a href="">bitlab</a></h1></div>
     <div id="login-toolbar">
         <form>
-            <input type="text" placeholder="Username">
-            <input type="password" placeholder="Password">
+            <input type="text" id="txt_uname" name="txt_uname" placeholder="Username">
+            <input type="password" id="txt_uname" name="txt_pwd" placeholder="Password">
             <button type="submit"><span class="material-icons-sharp">login</span></button>
         </form>
     </div>
