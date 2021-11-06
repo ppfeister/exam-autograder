396012a2903baf2aadda3558b56ac5815736cb85
Ahmed Faisal <80870776+AhmedAbdulFaisal@users.noreply.github.com>
2021-11-06 15:22:18 -0400

    Revert "added new config files, this branch is synced with develop, ignore base_ahmed"
    This reverts commit 901fdd451b15d8e947d26fc99e0ca5e4f76915c7.

--- index.php
+++ index.php
@@ -1,59 +1,15 @@
-<!-- Hypothetical Table Code
-    CREATE TABLE users (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-);
---> 
-
-<?php
-
-require_once 'dbconfig.php';
-
-if(isset($_POST['but_submit']) {
-    $uname = mysqli_real_escape_string($con,$_POST['txt_uname']);
-    $password = mysqli_real_escape_string($con,$POST['txt_pwd']);
-
-    if ($uname != "" && $password != "") {
-        //check for passwords within system
-
-        $sql_query = "select count(*) as cntUser 
-            from user 
-            where username='".$uname."' and password='".$password."'";
-        $resuilt = mysqli_query($con,$sql_query);
-        $row = mysqli_query($con,$sql_query);
-        $count = $row['cntUser'];
-
-        if ($count > 0) {
-            //set page to courses page
-        }else {
-            echo "wrong password lmao";
-        }
-    }
-})
-
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Coding examination website with automatic grading of student-submitted code">
-    <title>Coding Exam Software</title>
+    <title>bitlab - Student Coding Lab</title>
     <link rel="stylesheet" href="styles/master.css" type="text/css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Sharp" rel="stylesheet">
 </head>
 <body>
-    <header>
-        <div><h1><a href="">Bitlab</a></h1></div>
-        <div id="login-toolbar">
-            <form>
-                <input type="text" id="txt_uname" name="txt_uname" placeholder="Username">
-                <input type="password" id="txt_uname" name="txt_pwd" placeholder="Password">
-            </form>
-        </div>
-    </header>
+<?php require_once('common/header-login.php'); ?>
+<?php require_once('common/footer.php'); ?>
 </body>
 </html>
