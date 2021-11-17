a1d69c37bfd63f9207e3087fe2ff6e05e82d7d21
Ahmed Abdullah Faisal <ahmedabdulfaisal@gmail.com>
2021-11-16 19:29:38 -0500

    added stuff

--- common/header-login.php
+++ common/header-login.php
@@ -23,7 +23,7 @@ if ($form_user != "" && $form_pass != "") {
 <header>
     <div><h1><a href="">bitlab</a></h1></div>
     <div id="login-toolbar">
-        <form>
+        <form method="POST">
             <input type="text" id="txt_uname" name="txt_uname" placeholder="Username">
             <input type="password" id="txt_uname" name="txt_pwd" placeholder="Password">
             <button type="submit"><span class="material-icons-sharp">login</span></button>
