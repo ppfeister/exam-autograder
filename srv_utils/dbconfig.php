0cbdea83ef0291f9f89c233b8ada0b1b054e00df
Paul Pfeister <paul@pfeister.dev>
2021-11-18 14:55:54 -0500

    Revert from diag cred

--- srv_utils/dbconfig.php
+++ srv_utils/dbconfig.php
@@ -1,14 +1,8 @@
 <?php
-/*$host = getenv("MYSQL_PROD_URI");
+$host = getenv("MYSQL_PROD_URI");
 $dbname = "bitlab";
 $username = getenv("MYSQL_PROD_USER");
-$password = getenv("MYSQL_PROD_TOK");*/
+$password = getenv("MYSQL_PROD_TOK");
 
-
-$host = "prod.mysql.bitlab.pfei.cc";
-$dbname = "bitlab";
-$username = "bitlab-srv";
-$password = "rHG0YySFgTv3KBt5";
-
-$con = mysqli_connect($host, $username, $password, $dbname);
+$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 ?>
