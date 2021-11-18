3ac5adb2722a90145a394f372d562abe4bfbc6c1
Paul Pfeister <paul@pfeister.dev>
2021-11-17 19:28:43 -0500

    Add sign out button

--- common/header-internal.php
+++ common/header-internal.php
@@ -1,5 +1,10 @@
 <?php session_start(); ?>
 <header>
     <div><h1><a href="">bitlab</a></h1></div>
+    <div id="login-toolbar">
+        <form method="post">
+            <button type="submit" name="submit"><span style="padding-right: 0.5em;">Sign out</span><span class="material-icons-sharp">login</span></button>
+        </form>
+    </div>
 </header>
 <div class="page-wrapper"> <!-- closed in footer -->
