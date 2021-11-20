bcf56b20c4fbb3079cd8a7745925928bf87aa47c
Paul Pfeister <paul@pfeister.dev>
2021-11-20 08:28:19 -0500

    Disable sidebar

--- common/sidebar-left.php
+++ common/sidebar-left.php
@@ -1,4 +1,7 @@
-<div id="sidebar-left">
-    <a href="#">Link item a</a><br>
-    <a href="#">Link item b</a>
-</div>
+<!---------
+When sidebar begins being used...
+( 1 ) Uncomment `.page-wrapper{grid_template_columns}` in master.css to show on page
+( 2 ) Uncomment `#sidebar-left{padding}` in sidebar.css to show ~properly~
+----------->
+
+<div id="sidebar-left"></div>
