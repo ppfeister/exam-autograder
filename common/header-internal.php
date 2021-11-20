c5ecd00a40ccaa2f0f1c4d9f9a5c09b29aabce4a
Paul Pfeister <paul@pfeister.dev>
2021-11-20 04:10:45 -0500

    Update fonts

--- common/header-internal.php
+++ common/header-internal.php
@@ -2,7 +2,7 @@
     <div><h1><a href="">&lt;bitlab/&gt;</a></h1></div>
     <div id="login-toolbar">
         <form action="/profile/logout.php">
-            <button type="submit"><span style="padding-right: 0.5em;"><?php echo($_SESSION['first_name'] . " " . $_SESSION['last_name']);?></span><span class="material-icons-sharp">logout</span></button>
+            <button type="submit"><span style="padding-right: 0.5em; padding-top: 0.15em;"><?php echo($_SESSION['first_name'] . " " . $_SESSION['last_name']);?></span><span class="material-icons-sharp">logout</span></button>
         </form>
     </div>
 </header>
