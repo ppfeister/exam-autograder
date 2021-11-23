9b1801d8ecbb06fb19c873e003a359e453fd0d94
Paul Pfeister <paul@pfeister.dev>
2021-11-22 22:05:35 -0500

    Use Piston API for sandboxing

--- assignments/index.php
+++ assignments/index.php
@@ -25,7 +25,7 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/srv_utils/dbconfig.php");
 
 $userguid = $_SESSION['guid'];
 $assignment_info = mysqli_fetch_array(mysqli_query($con, "SELECT course.`Course Code`, assignments.`Assignment name` from `courses`.`assignments` as assignments INNER JOIN `courses`.`available-courses` as course INNER JOIN `courses`.`course-membership` as membership WHERE assignments.`Assignment ID` = $req_aid AND assignments.`Course GUID` = membership.`Course GUID` = course.`Course GUID` AND membership.`Member GUID` = $userguid;"));
-$assignment_query = mysqli_query($con, "SELECT assignments.`Assignment ID`, assignments.`Assignment name`, course.`Course Code`, qset.`Question ID`, qset.`Points`, questions.`Question Name`, questions.`Question Prompt`, questions.`Question Skeleton`, questions.`Code Language`, questions.`Question Tests`, questions.`Question Validation` from `courses`.`assignments` as assignments INNER JOIN `courses`.`available-courses` as course INNER JOIN `courses`.`course-membership` as membership INNER JOIN `courses`.`question-sets` as qset INNER JOIN `courses`.`saved-questions` as questions WHERE assignments.`Assignment ID` = qset.`Assignment ID` = $req_aid AND assignments.`Course GUID` = membership.`Course GUID` = course.`Course GUID` AND membership.`Member GUID` = $userguid AND qset.`Question ID` = questions.`Question ID`;");
+$assignment_query = mysqli_query($con, "SELECT assignments.`Assignment ID`, assignments.`Assignment name`, course.`Course Code`, qset.`Question ID`, qset.`Points`, questions.`Question Name`, questions.`Question Prompt`, questions.`Question Skeleton`, questions.`Code Language`, languages.`Ace`, questions.`Question Tests`, questions.`Question Validation` from `courses`.`assignments` as assignments INNER JOIN `courses`.`available-courses` as course INNER JOIN `courses`.`course-membership` as membership INNER JOIN `courses`.`languages-avail` as languages INNER JOIN `courses`.`question-sets` as qset INNER JOIN `courses`.`saved-questions` as questions WHERE assignments.`Assignment ID` = qset.`Assignment ID` = $req_aid AND assignments.`Course GUID` = membership.`Course GUID` = course.`Course GUID` AND membership.`Member GUID` = $userguid AND qset.`Question ID` = questions.`Question ID` AND questions.`Code Language` = languages.`Human-readable`;");
 $questions = [];
 while($question = mysqli_fetch_array($assignment_query))
     $questions[] = $question;
@@ -48,7 +48,7 @@ while($question = mysqli_fetch_array($assignment_query))
                 <span style="grid-area: left-fill;"></span>
                 <!--<span style="grid-area: right-fill;"></span>-->
                 <!--<a href="#" style="grid-area: reset;" title="Revert to skeleton"><span class="material-icons-sharp">restart_alt</span></a>-->
-                <a href="#" style="grid-area: stop;" title="Abort run-away code"><span class="material-icons-sharp">stop</span></a>
+                <a href="#" style="grid-area: stop;" title="Abort"><span class="material-icons-sharp">stop</span></a>
                 <a href="#" style="grid-area: run;" title="Run"><span class="material-icons-sharp">play_arrow</span></a>
                 <!--<a href="/assignments?aid=000001,q=1" style="grid-area: next-q-submit;" title="Next question">Next <span class="material-icons-sharp">arrow_forward</span></a>-->
                 <a href="#" onclick="submit_q$question[3]()" style="grid-area: next-q-submit;" title="Submit assignment">Submit <span class="material-icons-sharp">done</span></a>
@@ -59,11 +59,11 @@ while($question = mysqli_fetch_array($assignment_query))
             var editor_$question[3] = ace.edit("editor-$question[3]");
             editor_$question[3].setShowPrintMargin(false);
             editor_$question[3].setTheme("ace/theme/eclipse");
-            editor_$question[3].session.setMode("ace/mode/$question[8]");
+            editor_$question[3].session.setMode("ace/mode/$question[9]");
             
             function submit_q$question[3]() {
                 var ajax = new XMLHttpRequest();
-                var data = [$question[0], $question[3], encodeURIComponent(editor_$question[3].getValue())];
+                var data = [$question[0], $question[3], JSON.stringify(editor_$question[3].getValue())];
                 ajax.open("POST", "./submit-answer.php");
                 ajax.setRequestHeader("Content-type", "application/json");
                 ajax.send(JSON.stringify(data));
--- assignments/submit-answer.php
+++ assignments/submit-answer.php
@@ -1,7 +1,43 @@
 <?php
+require_once($_SERVER['DOCUMENT_ROOT'] . "/srv_utils/dbconfig.php");
+
 session_start();
 $data = json_decode(file_get_contents("php://input"), true);
-error_log("ses: " . $_SESSION['guid'], 0);
-error_log("aid: " . $data[0], 0);
-error_log("qid: " . $data[1], 0);
-error_log("ans: " . $data[2], 0);
+
+$piston_query = mysqli_fetch_array(mysqli_query($con, "SELECT languages.`Piston name`, languages.`Piston version`, languages.`File extension` from `courses`.`languages-avail` as languages INNER JOIN `courses`.`saved-questions` as questions WHERE questions.`Question ID` = 1 AND questions.`Code Language` = languages.`Human-readable`;"));
+$piston_lang = strval($piston_query[0]);
+$piston_version = strval($piston_query[1]);
+$piston_ext = strval($piston_query[2]);
+
+$sandbox_host = "https://emkc.org/api/v2/piston/execute";
+
+$headers = array(
+    "Accept: application/json",
+    "Content-Type: application/json"
+);
+
+$user_answer = <<< ANSWER
+{
+    "language": "$piston_lang",
+    "version": "$piston_version",
+    "files": [
+        {
+            "name": "answer$piston_ext",
+            "content": $data[2]
+        }
+    ]
+}
+ANSWER;
+
+
+$curl = curl_init($sandbox_host);
+curl_setopt($curl, CURLOPT_URL, $sandbox_host);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $user_answer);
+
+$resp = curl_exec($curl);
+curl_close($curl);
+
+error_log("piston result: " . $resp);
