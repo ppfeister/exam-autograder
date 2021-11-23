9a98b7e38e390de987e3829c3973ca0bcbbddd2f
Paul Pfeister <paul@pfeister.dev>
2021-11-23 18:02:43 -0500

    Fix auto-population of assignments and creation of assignments

--- assignments/instructor-toolbar.php
+++ assignments/instructor-toolbar.php
@@ -3,6 +3,6 @@
 echo <<< EOT
 <span class="instructor-toolbar">
     <a href="/assignments/new/?cid=$course[0]">New assignment</a>
-    <a href="/instructors/question-bank">Question Bank</a>
+    <a href="/instructors/new-question/">Question Bank</a>
 </span>
 EOT;
