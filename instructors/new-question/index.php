ec5792c1df0d8dbc874dc95e609db04e5d48d6f9
Paul Pfeister <paul@pfeister.dev>
2021-12-07 17:46:25 -0500

    Update question bank page

--- instructors/new-question/index.php
+++ instructors/new-question/index.php
@@ -2,6 +2,19 @@
 session_start();
 if(!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] == false)
     header("location: /");
+
+require_once($_SERVER['DOCUMENT_ROOT'] . "/srv_utils/dbconfig.php");
+$userguid = (int)$_SESSION['guid'];
+
+if(isset($_POST['submit'])) {
+    $question_name = $_POST['qname'];
+    $question_prompt = $_POST['question-prompt'];
+    $skeleton = $_POST['question-skeleton'];
+    $correct_output = $_POST['question-output'];
+    $question_lang = $_POST[''];
+
+    mysqli_query($con, "INSERT INTO `courses`.`saved-questions` (`Question Owner`, `Question Name`, `Question Prompt`, `Question Skeleton`, `Code Language`, `Question Tests`, `Question Validation`) VALUES ($userguid, $question_name,$question_prompt,$skeleton,'Python','',$correct_output);");
+}
 ?>
 
 <!DOCTYPE html>
@@ -12,62 +25,72 @@ if(!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] == false)
     <meta name="description" content="Coding examination website with automatic grading of student-submitted code">
     <title>bitlab - Student Coding Lab</title>
     <link rel="stylesheet" href="../../styles/master.css" type="text/css">
+    <link rel="stylesheet" href="./question-bank.css" type="text/css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Sharp" rel="stylesheet">
 </head>
     <body>
     <?php 
     require_once($_SERVER['DOCUMENT_ROOT'] . "/common/header-internal.php");
     require_once($_SERVER['DOCUMENT_ROOT'] . "/common/sidebar-left.php");
-    require_once($_SERVER['DOCUMENT_ROOT'] . "/srv_utils/dbconfig.php"); 
-    
-    $assignment_info = mysqli_fetch_array(mysqli_query($con, "SELECT course.`Course Code`, assignments.`Assignment name`, assignments.`Allow resubmit` from `courses`.`assignments` as assignments INNER JOIN `courses`.`available-courses` as course INNER JOIN `courses`.`course-membership` as membership WHERE assignments.`Assignment ID` = $req_aid AND assignments.`Course GUID` = membership.`Course GUID` = course.`Course GUID` AND membership.`Member GUID` = $userguid;"));
-    $assignment_query = mysqli_query($con, "SELECT assignments.`Assignment ID`, assignments.`Assignment name`, course.`Course Code`, qset.`Question ID`, qset.`Points`, questions.`Question Name`, questions.`Question Prompt`, questions.`Question Skeleton`, questions.`Code Language`, languages.`Ace`, questions.`Question Tests`, questions.`Question Validation` from `courses`.`assignments` as assignments INNER JOIN `courses`.`available-courses` as course INNER JOIN `courses`.`course-membership` as membership INNER JOIN `courses`.`languages-avail` as languages INNER JOIN `courses`.`question-sets` as qset INNER JOIN `courses`.`saved-questions` as questions WHERE assignments.`Assignment ID` = qset.`Assignment ID` AND assignments.`Assignment ID` = $req_aid AND assignments.`Course GUID` = membership.`Course GUID` = course.`Course GUID` AND membership.`Member GUID` = $userguid AND qset.`Question ID` = questions.`Question ID` AND questions.`Code Language` = languages.`Human-readable`;");
-    $questions = [];
-    while($question = mysqli_fetch_array($assignment_query)) {
+
+    $saved_questions_query = mysqli_query($con, "SELECT `Question ID`, `Question Name`, `Question Prompt`, `Question Skeleton`, `Code Language` FROM `courses`.`saved-questions` WHERE `Question Owner` = $userguid AND `Banked` = true;");
+    $saved_questions = [];
+    while($question = mysqli_fetch_array($saved_questions_query))
         $questions[] = $question;
-    }
+
+    $langs_avail_query = mysqli_query($con, "SELECT `Human-readable` FROM `courses`.`languages-avail`;");
+    $langs_avail = [];
+    while($lang = mysqli_fetch_array($langs_avail_query))
+        $langs_avail[] = $lang[0];
     ?>
-    <!--<div class="section-menu"></div>-->	
-	
-    <div class="split left">
-        <div class="section-main">
-            <div class="subsection-level1">
-                <h3>Setup New Question</h3>
-                <form method=post>
-                    <label for=question-prompt>Question Prompt:</label>
-                    <input class="textbox-small" id="question-prompt" type="text">
-                    <br> 
-                    <label for=lang>Language for Code: </label><select name="lang" id="pro-lang">
-                        <option value="lang1">Assembly</option>
-                        <option value="lang2">LISP</option>
-                    </select>
-                    <br>
-                    <label for=question-skeleton>Question Skeleton Code</label> <br>
-                    <textarea class=textarea id="skeleton" name="question-skeleton"> </textarea>
-                    <br>
-                </form>            
-            </div>
-            <div class="subsection-level1">
-                <form method=post>
-                    <label for=question-arguments>Program Arguments:</label>
-                    <input class="textbox-small" id="question-arguments" type="text">
+    <!--<div class="section-menu"></div>-->
+    <div class="section-main">
+        <h2>Question Bank</h2>
+        <div class="subsection-level1">
+            <form method=post>
+                <input type="text" name="qname" placeholder="(enter question title here)" size="50">
 
-                    <br> <label for=question-correctoutput>Correct Output</label> <br>
-                    <textarea class=textarea id="output" name="question-output"> </textarea>
-                </form>
-            </div>
-        </div>
-        
-    </div>
-    <div class="split right">
-        <div class="section-main">
-            <h3>Question Bank</h3>
-            <div class=sidebar-left>
+                <label for=lang>Language for Code: </label>
+                <select name="lang" id="program-lang">
+                    <?php
+                    foreach($langs_avail as $lang)
+                        echo "<option value=\"$lang\">$lang</option>";
+                    ?>
+                </select>
 
-            </div>
+                <label for=question-prompt>Question Prompt</label>
+                <textarea id="prompt" name="question-prompt" placeholder="(please enter your question prompt here)" style="height: 7em;""></textarea>
+
+                <label for=question-skeleton>Question Skeleton Code</label>
+                <textarea id="skeleton" name="question-skeleton" placeholder="Question Skeleton" style="height: 15em;"></textarea>
+
+                <label for=question-arguments>Program Arguments:</label>
+                <textarea id="question-arguments" name="question-arguments" placeholder="(please enter your arguments here)" style="height: 7em;"></textarea>
+
+                <label for=question-correctoutput>Correct Output</label> <br>
+                <textarea id="output" name="question-output" placeholder="Output" style="height: 7em;"></textarea>
+
+                <button type="submit" name="submit">Submit</span></button>
+            </form>
         </div>
     </div>
-	
-    <?php require_once('../common/footer.php'); ?>
+    <div id="sidebar-right">
+        <h2>Stored</h2>
+        <?php
+        if(empty($questions)) {
+            echo("No banked questions");
+        } else {
+            foreach($questions as $question) {
+                echo <<< EOT
+                <div class="qitem">
+                    <div class="qitem-name">$question[1]</div>
+                    <div class="qitem-lang">$question[4]</div>
+                </div>
+                EOT;
+            }
+        }
+        ?>
+    </div>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'] . "/common/footer.php"); ?>
 </body>
 </html>
